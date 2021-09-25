35eaa765efb56ff1cff57f5f31410d0053a5c5f8
djibadiawara <djiba.diawara97@gmail.com>
2021-09-25 18:57:28 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+var a, b
+
+function add(a, b) {
+    return (a + b);
+  }
+  function subtract(a, b) {
+    return (a - b);
+  }
+  function multiply(a, b) {
+    return (a * b);
+  }
+  function divide(a, b) {
+    return (a / b);
+  }
+  
+  function increment(a) {
+    return (a +1);
+  }
+  function decrement(a) {
+    return (a -1);
+  }
+ 
+  var n 
+  function makeInt(n) {
+    n=parseInt(n, 10);
+    return n;
+  }
+   
+  function preserveDecimal(n) {
+    n=parseFloat(n, '2.222');
+    return n;
+  } 
+  
